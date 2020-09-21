2d48bdb53b252ed93407d6331cd3c5314a80e37e
pinkmanliu <1007545739@qq.com>
2020-09-21 20:06:17 +0800

    成功搭建pms和sms工程

---
+++ gmall-pms/src/main/java/com/atguigu/gmall/pms/mapper/BrandMapper.java
@@ -0,0 +1,17 @@
+package com.atguigu.gmall.pms.mapper;
+
+import com.atguigu.gmall.pms.entity.BrandEntity;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * 品牌
+ * 
+ * @author xiaoliu
+ * @email xiaoliu@atguigu.com
+ * @date 2020-09-21 19:17:02
+ */
+@Mapper
+public interface BrandMapper extends BaseMapper<BrandEntity> {
+	
+}
---
+++ gmall-pms/src/main/java/com/atguigu/gmall/pms/mapper/SkuAttrValueMapper.java
@@ -0,0 +1,17 @@
+package com.atguigu.gmall.pms.mapper;
+
+import com.atguigu.gmall.pms.entity.SkuAttrValueEntity;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * sku销售属性&值
+ * 
+ * @author xiaoliu
+ * @email xiaoliu@atguigu.com
+ * @date 2020-09-21 19:17:02
+ */
+@Mapper
+public interface SkuAttrValueMapper extends BaseMapper<SkuAttrValueEntity> {
+	
+}
